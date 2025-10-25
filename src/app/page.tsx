6a64a3cb833eed8d1cc0d1e7512ef0937dceab68
Javier Vargas <javierdavidvargas9@gmail.com>
2025-10-25 14:46:40 -0300

    frontend update
    sidebar added
    3d nextbot added
    main buttons added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,33 @@
+import Spline from "@splinetool/react-spline/next";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { Input } from "@/components/ui/input";
+import { Mic } from "lucide-react";
+
 export default function Home() {
-  return <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">hola</div>;
+  return (
+    <div className="flex flex-col relative h-screen items-center justify-center font-sans p-0">
+      <SidebarProvider
+        style={
+          {
+            "--sidebar-width": "19rem",
+          } as React.CSSProperties
+        }
+      >
+        <AppSidebar />
+        <SidebarInset>
+          <Spline scene="https://prod.spline.design/1ff7UimOLWlh5QfW/scene.splinecode" />
+          <header className="flex h-16 shrink-0 items-center gap-2 px-2 absolute top-0">
+            <SidebarTrigger className="-ml-1" />
+          </header>
+          <div className="w-full h-20 absolute bottom-0 p-2 gap-2 flex">
+            <Input placeholder="Pregunta lo que quieras" className="w-full h-full rounded-full bg-zinc-100/10 border-zinc-100/20"></Input>
+            <button className="h-full w-20 flex items-center justify-center rounded-full bg-zinc-100/10 border-zinc-100/20 border-1">
+              <Mic className="w-5 h-5 text-zinc-500" />
+            </button>
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+    </div>
+  );
 }
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import { GalleryVerticalEnd } from "lucide-react";
+
+import { Sidebar, SidebarContent, SidebarGroup, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+
+// This is sample data.
+const data = {
+  navMain: [
+    {
+      title: "Getting Started",
+      url: "#",
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar variant="floating" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                  <GalleryVerticalEnd className="size-4" />
+                </div>
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-medium">Documentation</span>
+                  <span className="">v1.0.0</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu className="gap-2">
+            {data.navMain.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url} className="font-medium">
+                    {item.title}
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
