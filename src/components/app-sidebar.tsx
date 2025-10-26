82848fda3ce515134b469e4c6c87c7e7bdf1be94
Drex <120832806+drexiing@users.noreply.github.com>
2025-10-26 14:32:43 -0300

    End contributors

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,18 +1,8 @@
-import { Link } from "react-router-dom";
+import Link from "next/link";
 import * as React from "react";
-import { BotMessageSquare } from "lucide-react";
+import { BotMessageSquare, PersonStandingIcon } from "lucide-react";
 
 import { Sidebar, SidebarContent, SidebarGroup, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
-
-// This is sample data.
-const data = {
-  navMain: [
-    {
-      title: "Getting Started",
-      url: "#",
-    },
-  ],
-};
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
@@ -21,7 +11,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
-              <Link to="/">
+              <Link href="/">
                 <div className="bg-[#e16e09] text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
                   <BotMessageSquare className="size-5" />
                 </div>
@@ -32,23 +22,21 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
               </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <Link href="/contributors">
+                <div className="bg-[#e16e09] text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                  <PersonStandingIcon className="size-5" />
+                </div>
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-medium">Contribuidores del Proyecto</span>
+                  <span className="font-light">Quienes estamos detrás</span>
+                </div>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarMenu className="gap-2">
-            {data.navMain.map((item) => (
-              <SidebarMenuItem key={item.title}>
-                <SidebarMenuButton asChild>
-                  <a href={item.url} className="font-medium">
-                    {item.title}
-                  </a>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            ))}
-          </SidebarMenu>
-        </SidebarGroup>
-      </SidebarContent>
     </Sidebar>
   );
 }
